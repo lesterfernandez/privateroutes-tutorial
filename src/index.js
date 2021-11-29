1abd40c9c3b35ade99cbeed48155942c3baa781a
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-29 10:20:57 -0500

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
